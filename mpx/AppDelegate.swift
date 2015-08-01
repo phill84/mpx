2214681a54e3d3554d359d78325334485b600242
Jiening Wen <phill84@phill84.org>
2015-08-01 14:45:45 +0200

    Initial Commit

---
+++ mpx/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  mpx
+//
+//  Created by Jiening Wen on 01/08/15.
+//  Copyright (c) 2015 phill84. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(aNotification: NSNotification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(aNotification: NSNotification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
---
+++ mpx/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  mpx
+//
+//  Created by Jiening Wen on 01/08/15.
+//  Copyright (c) 2015 phill84. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: AnyObject? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
